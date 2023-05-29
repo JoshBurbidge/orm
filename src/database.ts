830c9a4963505f141fee41b1cf796bdb248e04e8
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2023-05-28 19:11:19 -0500

    add test class

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,33 @@
-import mysql from 'mysql2';
+import mysql, { RowDataPacket, QueryError } from 'mysql2';
+import { User, UserOptions } from './client/model/User';
+
+function createUserOptions(userRowData: RowDataPacket): UserOptions {
+  return {
+    id: userRowData.id,
+    username: userRowData.username,
+    name: userRowData.name,
+    type: userRowData.type
+  };
+}
 
 export async function connect() {
   const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: process.env.DB_USER,
     database: 'test',
     password: process.env.DB_PASSWORD
   });
 
   connection.query(
     'SELECT * FROM `user`',
-    function(err, results, fields) {
-      if (err) console.log(err);
+    function(err: QueryError, results: RowDataPacket) {
+      if (err) {
+        console.log(err);
+        return;
+      }
       console.log(results); // results contains rows returned by server
+      const userOptions = createUserOptions(results[0]);
+      console.log(new User(userOptions));
       // console.log(fields); // fields contains extra meta data about results, if available
     }
   );
